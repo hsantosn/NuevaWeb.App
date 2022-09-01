3dcd9eef34b345d8168c3b1b192eef00214e9639
Harold <hsantos.navarro@gmail.com>
2022-08-31 21:48:33 -0500

    segunda version

---
+++ NuevaWeb.App.Persistencia/IRepositorioSaludos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NuevaWeb.App.Dominio;
+
+namespace NuevaWeb.App.Persistencia
+{
+    public interface IRepositorioSaludos
+    {
+        IEnumerable<Saludo> GetAll();
+    }
+}
---
+++ NuevaWeb.App.Persistencia/RepositorioSaludos.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NuevaWeb.App.Dominio;
+
+namespace NuevaWeb.App.Persistencia
+{
+    public class IRepositorioSaludos : IRepositorioSaludos
+    {
+        List<Saludo> saludos;
+    }
+    public RepositorioSaludos()
+    {
+        saludos = new List<Saludo>()
+        {
+            new Saludo{Id=1,Italiano = "Buongiorno", Japones = "Ohayo", Ruso ="dobroye utro"},
+            new Saludo{Id=2,Italiano = "Buon Pomeriggio", Japones = "Kon'nichiwa", Ruso ="Ddobryy den"},
+            new Saludo{Id=3,Italiano = "Buona Notte", Japones = "Oyasumi", Ruso ="Spokoynoy nochi"},
+        };
+    }
+    public IEnumerable<Saludo> GetAll()
+    {
+        return saludos;
+    }
+
+}
