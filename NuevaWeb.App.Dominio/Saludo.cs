139ffa663b8cb75baac0f44beb2c4abe3677785d
Harold <hsantos.navarro@gmail.com>
2022-08-31 21:48:05 -0500

    segunda version

---
+++ NuevaWeb.App.Dominio/Saludo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NuevaWeb.App.Dominio
+{
+    public class Saludo
+    {
+        public int Id { get; set; }
+        public string Italiano { get; set; }
+        public string Japones {get; set;}
+        public string Ruso {get; set;}
+    }
+}
